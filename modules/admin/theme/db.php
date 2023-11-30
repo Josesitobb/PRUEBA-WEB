36e5116d6f8a7426e1dd0a1119849665a4c8fc38
Josesitobb <124468479+Josesitobb@users.noreply.github.com>
2023-11-30 15:21:07 -0500

    base de datos azure

--- modules/admin/theme/db.php
+++ modules/admin/theme/db.php
@@ -1,16 +1,54 @@
 <?php
 
-$servername="localhost";
-$username ="root";
-$password ="";
-$db="mydb";
-$port= "3306";
+// $servername="localhost";
+// $username ="root";
+// $password ="";
+// $db="mydb";
+// $port= "3306";
 
-$conn=mysqli_connect($servername,$username,$password,$db,$port);
+// $conn=mysqli_connect($servername,$username,$password,$db,$port);
 
 // Probar la conexion
 // if(!$conn){
 //     die("Muy bot".mysqli_connect_error());
 // }
 // echo "La conexion esta muy fina"
-// ?>
+
+
+// 
+
+
+$servername = "josecitas.mysql.database.azure.com";
+$username = "JOSE";
+$password = "Celular528";
+$db = "mydb";
+$port = 3306;
+
+// Ruta al archivo de certificado de Azure (puede variar según tu configuración)
+$ssl_cert = '../../../base _de_datos/DigiCertGlobalRootCA.crt.pem"';
+
+
+
+// Configuración de la conexión
+$conn = mysqli_init();
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+// Establecer la ruta del certificado
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
+
+// Conectar a la base de datos
+mysqli_real_connect($conn, $servername, $username, $password, $db, $port);
+
+// Probar la conexión
+if($conn){
+    echo "hay conexion";
+}else{
+    echo "no hay conexion";
+}
+
+// Realizar operaciones en la base de datos aquí
+
+
+
+?>
+
